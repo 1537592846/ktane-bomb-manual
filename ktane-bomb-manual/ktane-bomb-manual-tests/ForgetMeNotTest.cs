49ca64ccde7628b1167dae87617e3331eaeaf9a7
André Soares França <andre.franca@globalweb.com.br>
2019-09-12 12:42:40 -0400

    Added forget me not

---
+++ ktane-bomb-manual/ktane-bomb-manual-tests/ForgetMeNotTest.cs
@@ -0,0 +1,147 @@
+﻿using ktane_bomb_manual;
+using ktane_bomb_manual.Modules;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class ForgetMeNotTest
+    {
+        public Bomb bomb;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SerialOnlyOddBombTest()
+        {
+            bomb = new Bomb();
+            bomb.Serial = "QS1LN4";
+            bomb.BatteryAA = 4;
+            bomb.Indicators.Add(new Indicator("sig", false));
+            bomb.Indicators.Add(new Indicator("thn", true));
+            bomb.Ports.Add(new Port("dvi", 1));
+
+            var forgetMeNotModule = new ForgetMeNot();
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "3");
+
+            Assert.AreEqual("Listing the number, pay attention. 3, 9, 8 next. 1, 4, 8 next. 0, 7 done.", forgetMeNotModule.Solve(bomb), "Error test #1", null);
+
+            bomb = new Bomb();
+            bomb.Serial = "TH8ZK9";
+            bomb.BatteryAA = 4;
+            bomb.BatteryD = 1;
+            bomb.Indicators.Add(new Indicator("msa", true));
+            bomb.Ports.Add(new Port("parallel", 1));
+
+            forgetMeNotModule = new ForgetMeNot();
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "2");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "2");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "2");
+            forgetMeNotModule.AddNumber(bomb, "2");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "4");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "4");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "4");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "4");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "4");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "2");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "4");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "2");
+            forgetMeNotModule.AddNumber(bomb, "1");
+            forgetMeNotModule.AddNumber(bomb, "4");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "8");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "7");
+            forgetMeNotModule.AddNumber(bomb, "6");
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "3");
+            forgetMeNotModule.AddNumber(bomb, "0");
+            forgetMeNotModule.AddNumber(bomb, "9");
+            forgetMeNotModule.AddNumber(bomb, "5");
+            forgetMeNotModule.AddNumber(bomb, "9");
+
+            Assert.AreEqual("Listing the number, pay attention. 4, 1, 7 next. 9, 6, 2 next. 4, 4, 2 next. 0, 2, 9 next. 1, 3, 1 next. 1, 1, 3 next. 0, 8, 4 next. 6, 0, 1 next. 1, 8, 0 next. 5, 0, 5 next. 6, 6, 4 next. 2, 5, 1 next. 3, 0, 7 next. 9, 5, 1 next. 4, 1, 2 next. 7, 9, 4 next. 2, 8, 2 next. 4, 9, 5 next. 8, 1, 5 next. 7, 9, 6 next. 0, 7, 6 next. 5, 6, 7 next. 9, 6, 8 next. 7, 6, 3 next. 0, 7, 9 next. 1, 4, 3 next. 5, 2, 5 next. 9, 2, 5 next. 2, 2, 5 next. 5, 8, 8 next. 5, 0, 2 next. 9, 0, 4 next. 8 done.", forgetMeNotModule.Solve(bomb), "Error test #1", null);
+        }
+    }
+}
---
+++ ktane-bomb-manual/ktane-bomb-manual/Modules/ForgetMeNot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ktane_bomb_manual.Modules
+{
+    public class ForgetMeNot : Module
+    {
+        public ForgetMeNot()
+        {
+            EndNumbers = new List<string>();
+        }
+
+        public List<string> EndNumbers;
+
+        public override string Solve(Bomb bomb)
+        {
+            var message = "Listing the number, pay attention. ";
+
+            for (int i = 0; i < EndNumbers.Count; i++)
+            {
+                message += EndNumbers[i];
+                message += i % 3 == 2 ? " next. " : ", ";
+            }
+            while (!char.IsDigit(message.Last())) { message = message.Substring(0, message.Count() - 1); }
+
+            return message+" done.";
+        }
+
+        public void AddNumber(Bomb bomb, string number)
+        {
+            if (EndNumbers.Count == 0)
+            {
+                if (bomb.HasUnlitIndicator("car")) { EndNumbers.Add((2 + int.Parse(number)).ToString().Last().ToString()); return; }
+                if (bomb.GetUnlitIndicators() > bomb.GetLitIndicators()) { EndNumbers.Add((7 + int.Parse(number)).ToString().Last().ToString()); return; }
+                if (bomb.GetUnlitIndicators() == 0) { EndNumbers.Add((bomb.GetLitIndicators() + int.Parse(number)).ToString().Last().ToString()); return; }
+                EndNumbers.Add((bomb.GetLastSerialDigit() + int.Parse(number)).ToString().Last().ToString());
+                return;
+            }
+
+            if (EndNumbers.Count == 1)
+            {
+                if (bomb.HasPort("serial") && bomb.GetManyDigitsInSerial() >= 3) { EndNumbers.Add((3 + int.Parse(number)).ToString().Last().ToString()); return; }
+                if (int.Parse(EndNumbers[0]) % 2 == 0) { EndNumbers.Add((int.Parse(EndNumbers[0]) + 1 + int.Parse(number)).ToString().Last().ToString()); return; }
+                EndNumbers.Add((int.Parse(EndNumbers[0]) - 1 + int.Parse(number)).ToString().Last().ToString());
+                return;
+            }
+
+            if (EndNumbers[EndNumbers.Count - 1] == "0" || EndNumbers[EndNumbers.Count - 2] == "0") { EndNumbers.Add((bomb.GetBiggestSerialDigit() + int.Parse(number)).ToString().Last().ToString()); return; }
+            if (int.Parse(EndNumbers[EndNumbers.Count - 1]) % 2 == 0 && int.Parse(EndNumbers[EndNumbers.Count - 2]) % 2 == 0) { EndNumbers.Add(bomb.GetSmallestOddSerialDigit() != -1 ? (bomb.GetSmallestOddSerialDigit() + int.Parse(number)).ToString().Last().ToString() : (9 + int.Parse(number)).ToString().Last().ToString()); return; }
+            EndNumbers.Add((int.Parse((int.Parse(EndNumbers[EndNumbers.Count - 1]) + int.Parse(EndNumbers[EndNumbers.Count - 2])).ToString().Substring(0, 1)) + int.Parse(number)).ToString().Last().ToString());
+        }
+    }
+}
