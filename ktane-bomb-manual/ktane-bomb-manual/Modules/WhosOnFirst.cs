716c6580bf2d57cbafda9723b292d9ee1583960a
Ryuzaki <andrefranca86@gmail.com>
2019-09-22 19:14:24 -0400

    -Added whos first WIP

---
+++ ktane-bomb-manual/ktane-bomb-manual/Modules/WhosOnFirst.cs
@@ -0,0 +1,118 @@
+﻿namespace ktane_bomb_manual.Modules
+{
+    public class WhosOnFirst : Module
+    {
+        public bool FirstEntry;
+
+        public override string Solve(Bomb bomb)
+        {
+            return "Use normal commands for this module.";
+        }
+
+        public override string Command(Bomb bomb, string command)
+        {
+            if (command.Contains("solved"))
+            {
+                Solved = true;
+                return "Module solved.";
+            }
+
+            while (command.Split(' ')[0] != "is") command = command.Replace(command.Split(' ')[0], "").Trim();
+
+            return FirstEntry ? WhichPosition(command.Replace("is ", "")) : WhichWords(command.Replace("is ", ""));
+        }
+
+        public string WhichPosition(string word)
+        {
+            switch (word)
+            {
+                case "ur": return "Top left.";
+                case "first":
+                case "okay":
+                case "c": return "Top right.";
+                case "yes":
+                case "nothing":
+                case "led":
+                case "they are": return "Middle left.";
+                case "blank":
+                case "read":
+                case "red":
+                case "you":
+                case "your":
+                case "you're":
+                case "their": return "Middle right.";
+                case "":
+                case "reed":
+                case "leed":
+                case "they're": return "Bottom left.";
+                case "display":
+                case "says":
+                case "no":
+                case "lead":
+                case "hold on":
+                case "you are":
+                case "there":
+                case "see":
+                case "cee": return "Bottom right.";
+                default: return "";
+            }
+        }
+
+        public string WhichWords(string word)
+        {
+            switch (word)
+            {
+                case "READY": return "YES, OKAY, WHAT, MIDDLE, LEFT, PRESS, RIGHT, BLANK, READY, NO, FIRST, UHHH, NOTHING, WAIT";
+                case "FIRST": return "LEFT, OKAY, YES, MIDDLE, NO, RIGHT, NOTHING, UHHH, WAIT, READY, BLANK, WHAT, PRESS, FIRST";
+                case "NO": return "BLANK, UHHH, WAIT, FIRST, WHAT, READY, RIGHT, YES, NOTHING, LEFT, PRESS, OKAY, NO, MIDDLE";
+                case "BLANK": return "WAIT, RIGHT, OKAY, MIDDLE, BLANK, PRESS, READY, NOTHING, NO, WHAT, LEFT, UHHH, YES, FIRST";
+                case "NOTHING": return "UHHH, RIGHT, OKAY, MIDDLE, YES, BLANK, NO, PRESS, LEFT, WHAT, WAIT, FIRST, NOTHING, READY";
+                case "YES": return "OKAY, RIGHT, UHHH, MIDDLE, FIRST, WHAT, PRESS, READY, NOTHING, YES, LEFT, BLANK, NO, WAIT";
+                case "WHAT": return "UHHH, WHAT, LEFT, NOTHING, READY, BLANK, MIDDLE, NO, OKAY, FIRST, WAIT, YES, PRESS, RIGHT";
+                case "UHHH": return "READY, NOTHING, LEFT, WHAT, OKAY, YES, RIGHT, NO, PRESS, BLANK, UHHH, MIDDLE, WAIT, FIRST";
+                case "LEFT": return "RIGHT, LEFT, FIRST, NO, MIDDLE, YES, BLANK, WHAT, UHHH, WAIT, PRESS, READY, OKAY, NOTHING";
+                case "RIGHT": return "YES, NOTHING, READY, PRESS, NO, WAIT, WHAT, RIGHT, MIDDLE, LEFT, UHHH, BLANK, OKAY, FIRST";
+                case "MIDDLE": return "BLANK, READY, OKAY, WHAT, NOTHING, PRESS, NO, WAIT, LEFT, MIDDLE, RIGHT, FIRST, UHHH, YES";
+                case "OKAY": return "MIDDLE, NO, FIRST, YES, UHHH, NOTHING, WAIT, OKAY, LEFT, READY, BLANK, PRESS, WHAT, RIGHT";
+                case "WAIT": return "UHHH, NO, BLANK, OKAY, YES, LEFT, FIRST, PRESS, WHAT, WAIT, NOTHING, READY, RIGHT, MIDDLE";
+                case "PRESS": return "RIGHT, MIDDLE, YES, READY, PRESS, OKAY, NOTHING, UHHH, BLANK, LEFT, FIRST, WHAT, NO, WAIT";
+                case "YOU": return "SURE, YOU ARE, YOUR, YOU'RE, NEXT, UH HUH, UR, HOLD, WHAT?, YOU, UH UH, LIKE, DONE, U";
+                case "YOU ARE": return "YOUR, NEXT, LIKE, UH HUH, WHAT?, DONE, UH UN, HOLD, YOU, U, YOU'RE, SURE, UR, YOU ARE";
+                case "YOUR": return "UH UH, YOU ARE, UH HUH, YOUR, NEXT, UR, SURE, U, YOU'RE, YOU, WHAT?, HOLD, LIKE, DONE";
+                case "YOU'RE": return "YOU, YOU'RE, UR, NEXT, UH UH, YOU ARE, U, YOUR, WHAT?, UH HUH, SURE, DONE, LIKE, HOLD";
+                case "UR": return "DONE, U, UR, UH HUH, WHAT?, SURE, YOUR, HOLD, YOU'RE, LIKE, NEXT, UH UH, YOU ARE, YOU";
+                case "U": return "UH HUH, SURE, NEXT, WHAT?, YOU'RE, UR, UH UH, DONE, U, YOU, LIKE, HOLD, YOU ARE, YOUR";
+                case "UH HUH": return "UH HUH, YOUR, YOU ARE, YOU, DONE, HOLD, UH UH, NEXT, SURE, LIKE, YOU'RE, UR, U, WHAT?";
+                case "UH UH": return "UR, U, YOU ARE, YOU'RE, NEXT, UH UH, DONE, YOU, UH HUH, LIKE, YOUR, SURE, HOLD, WHAT?";
+                case "WHAT?": return "YOU, HOLD, YOU'RE, YOUR, U, DONE, UH UH, LIKE, YOU ARE, UH HUH, UR, NEXT, WHAT?, SURE";
+                case "DONE": return "SURE, UH HUH, NEXT, WHAT?, YOUR, UR, YOU'RE, HOLD, LIKE, YOU, U, YOU ARE, UH UH, DONE";
+                case "NEXT": return "WHAT?, UH HUH, UH UH, YOUR, HOLD, SURE, NEXT, LIKE, DONE, YOU ARE, UR, YOU'RE, U, YOU";
+                case "HOLD": return "YOU ARE, U, DONE, UH UH, YOU, UR, SURE, WHAT?, YOU'RE, NEXT, HOLD, UH HUH, YOUR, LIKE 0";
+                case "SURE": return "YOU ARE, DONE, LIKE, YOU'RE, YOU, HOLD, UH HUH, UR, SURE, U, WHAT?, NEXT, YOUR, UH UH";
+                case "LIKE": return "YOU'RE, NEXT, U, UR, HOLD, DONE, UH UH, WHAT?, UH HUH, YOU, LIKE, SURE, YOU ARE, YOUR ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                default: return "";
+            }
+        }
+    }
+}
